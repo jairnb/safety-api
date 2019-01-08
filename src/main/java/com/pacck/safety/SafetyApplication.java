4ac05761d9d3b81d3c3344ee7938277cf50d3af8
jairnb <jair10.nb@hotmail.com>
2019-01-08 11:06:32 -0800

    first commit

---
+++ src/main/java/com/pacck/safety/SafetyApplication.java
@@ -0,0 +1,14 @@
+package com.pacck.safety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyApplication.class, args);
+	}
+
+}
+
